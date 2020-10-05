fae619f8043f459e622313a32c2db56c7afc4628
Vladislav Khr <wolt6@mail.ru>
2020-10-05 22:06:58 +0300

    feat(Form): function call to render

--- src/Options/Form.php
+++ src/Options/Form.php
@@ -157,6 +157,8 @@ class Form
 								} else if (file_exists($opt['path'])) {
 									/** @noinspection PhpIncludeInspection */
 									include $opt['path'];
+								} else if (!empty($opt['fn']) && !is_string($opt['fn']) && is_callable($opt['fn'])) {
+									$opt['fn']();
 								}
 								?>
 								</td>
